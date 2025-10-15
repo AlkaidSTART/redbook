77c8cc3c1e91b49c2e5ef7549ad21701ccb6ce03
AlkaidSTART <2595006848@qq.com>
2025-10-15 19:25:52 +0800

    完成Body-1-box展示组件的重构

--- src/components/Body1-box.vue
+++ src/components/Body1-box.vue
@@ -1,49 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-import { watch } from 'vue'
-import Refersh from './refersh.vue'
-import body1box from './Body1-box.vue'
-const activeIndex = ref(0)
-const delayIndex = ref(0)
-const showRefersh = ref(false)
-const rotate = ref(false)
-const handleClick = (index) => {
-  activeIndex.value = index
-
-  // 每次点击都显示刷新动画
-  showRefersh.value = true
-  rotate.value = true
-  setTimeout(() => {
-    showRefersh.value = false
-    rotate.value = false
-  }, 1800)
-}
-rotate.value = true
-watch(activeIndex, (newVal) => {
-  setTimeout(() => {
-    delayIndex.value = newVal
-  }, 300)
-})
 </script>
 <template>
-
-  <nav class="nave">
-    <ul>
-      <li :class="{ 'active': activeIndex === 0, 'delayIndex': delayIndex === 0 }" @click="handleClick(0)">推荐</li>
-      <li :class="{ 'active': activeIndex === 1, 'delayIndex': delayIndex === 1 }" @click="handleClick(1)">穿搭</li>
-      <li :class="{ 'active': activeIndex === 2, 'delayIndex': delayIndex === 2 }" @click="handleClick(2)">美食</li>
-      <li :class="{ 'active': activeIndex === 3, 'delayIndex': delayIndex === 3 }" @click="handleClick(3)">彩妆</li>
-      <li :class="{ 'active': activeIndex === 4, 'delayIndex': delayIndex === 4 }" @click="handleClick(4)">影视</li>
-    </ul>
-  </nav>
- <div class="r" :class="{ 'hide': !showRefersh }">
-    <Refersh :rotate="rotate"></Refersh>
-  </div>
-  <div class="main" :class="{ 'moved-up': !showRefersh }">
-    <div class="1box">
-      <body1box></body1box>
-    </div>
-    <!-- <a class="container" href="#">
+<a class="container" href="#">
       <section class="a-s">
         <div class="banner">
           <img src="../assets/blank.png" alt="">
@@ -64,79 +22,16 @@ watch(activeIndex, (newVal) => {
           </div>
         </div>
       </section>
-    </a> -->
-  </div>
+    </a>
 </template>
 <style lang="scss" scoped>
 $dp: 0.1rem;
-
-img {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-
-a {
-  text-decoration: none;
-  color: #fff;
-}
-
-.nave {
-  width: 100%;
-  height: calc(10 * #{$dp});
-  // margin-bottom: calc(4 * #{$dp});
-  background-color: #363636;
-  font-size: 16px;
-
-  ul {
-    display: flex;
-  }
-
-  li {
-    flex: 1;
-    text-align: center;
-    color: #ffa2b6;
-    line-height: calc(10 * #{$dp});
-
-    &.active {
-      background-color: #141414;
-      border-radius: calc(2 * #{$dp});
-    }
-  }
-}
-
-.r {
-  transition: all 0.5s ease-in-out;
-  overflow: hidden;
-  max-height: 100px;
-
-  &.hide {
-    max-height: 0;
-    margin-top: 0;
-    opacity: 0;
-  }
-}
-
-.main {
-  width: 100%;
-  display: flex;
-  margin-top: calc(4 * #{$dp});
-  overflow-y: scroll;
-  overflow-x: hidden;
-  padding-left: calc(3 * #{$dp});
-  padding-right: calc(3 * #{$dp});
-  transition: margin-top 1.2s ease-in-out;
-
-  &.moved-up {
-    margin-top: calc(1 * #{$dp});
-  }
-
-  // flex-wrap: wrap;
   .container {
     width: calc(90* #{$dp});
     height: calc(140 * #{$dp});
     background-color: #363636;
     display: flex;
+    font-size: 16px;
     padding-top: calc(2 * #{$dp});
     padding-right: calc(2 * #{$dp});
 
@@ -152,7 +47,11 @@ a {
         background-color: #363636;
         border: 1px solid #fff;
         border-radius: calc(2 * #{$dp});
-
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
       }
 
       span {
@@ -243,5 +142,4 @@ a {
       }
     }
   }
-}
 </style>
